d1207ccc752989925082707a276696361a3df48e
Arthur Chan <64251502+arthchan@users.noreply.github.com>
2024-12-17 14:09:13 +0800

    Add reset of relays when offline

--- 7609/7609.ino
+++ 7609/7609.ino
@@ -18,7 +18,9 @@ char ssid[] = SSID;
 char pass[] = PASS;
 
 // Declare global variables for DI, DO and flags
-int P1, P2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, D17, D18;
+int P1 = 0;
+int P2 = 0;
+int D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16;
 int D19 = 0;
 bool high_level_flag = false;
 bool middle_level_flag = false;
@@ -34,57 +36,61 @@ bool p2_running_flag = false;
 // Create Blynk timer object
 BlynkTimer timer;
 
-// Write DI value to datastream
-void myTimer() 
+BLYNK_CONNECTED() {
+  // Reset control switches
+  Serial.println("Reset states of relays.");
+  Blynk.virtualWrite(V0, P1);   // Pump 1 Remote Run
+  Blynk.virtualWrite(V1, P2);   // Pump 2 Remote Run
+}
+
+// DO for Pump 1
+BLYNK_WRITE(V0)
 {
-  // Read DI values
-  D3 = digitalRead(A0);  // Pump 1 Power
-  Blynk.virtualWrite(V2, D3);
+  P1 = param.asInt();
+  if (P1 == 1) {
+    digitalWrite(8, HIGH);
+    Serial.println("PUMP 1 RUNNING (REMOTE)");
+  }
+  else {
+    digitalWrite(8, LOW);
+    Serial.println("PUMP 1 STOPPED (REMOTE)");
+  }
+}
 
-  D4 = digitalRead(A1);  // Pump 2 Power
-  Blynk.virtualWrite(V3, D4);
+// DO for Pump 2
+BLYNK_WRITE(V1)
+{
+  P2 = param.asInt();
+  if (P2 == 1) {
+    digitalWrite(12, HIGH);
+    Serial.println("PUMP 2 RUNNING (REMOTE)");
+  }
+  else {
+    digitalWrite(12, LOW);
+    Serial.println("PUMP 2 STOPPED (REMOTE)");
+  }
+}
 
-  D5 = digitalRead(0);   // Low Level
-  Blynk.virtualWrite(V4, D5);
-
-  D6 = digitalRead(1);   // Pump 1 Running
-  Blynk.virtualWrite(V5, D6);
-
-  D7 = digitalRead(2);   // Pump 2 Running
-  Blynk.virtualWrite(V6, D7);
-
-  D8 = digitalRead(3);   // Middle Level
-  Blynk.virtualWrite(V7, D8);
-
-  D9 = digitalRead(5);   // Pump 1 Common Fault
-  Blynk.virtualWrite(V8, D9);
-
-  D10 = digitalRead(6);  // Pump 2 Common Fault
-  Blynk.virtualWrite(V9, D10);
-
-  D11 = digitalRead(9);  // High Level
-  Blynk.virtualWrite(V10, D11);
-
-  D12 = digitalRead(10); // High Level Backup 1
-  Blynk.virtualWrite(V11, D12);
-
-  D13 = digitalRead(11); // High Level Backup 2
-  Blynk.virtualWrite(V12, D13);
-
-  D14 = digitalRead(13); // Long Running
-  Blynk.virtualWrite(V13, D14);
-
-  D15 = 1 - D6; // Pump 1 Stopped (Opposite of Running)
-  Blynk.virtualWrite(V14, D15);
-
-  D16 = 1 - D7; // Pump 2 Stopped (Opposite of Running)
-  Blynk.virtualWrite(V15, D16);
-
-  D17 = 1 - D3; // Pump 1 Power Loss (Opposite of Pump 1 Power)
-  Blynk.virtualWrite(V16, D17);
-
-  D18 = 1 - D4; // Pump 2 Power Loss (Opposite of Pump 2 Power)
-  Blynk.virtualWrite(V17, D18);
+// Write DI values to datastreams
+void myTimer()
+{
+  Blynk.beginGroup();
+  Blynk.virtualWrite(V2, D3);   // Pump 1 Power
+  Blynk.virtualWrite(V3, D4);   // Pump 2 Power
+  Blynk.virtualWrite(V4, D5);   // Low Level
+  Blynk.virtualWrite(V5, D6);   // Pump 1 Running
+  Blynk.virtualWrite(V6, D7);   // Pump 2 Running
+  Blynk.virtualWrite(V7, D8);   // Middle Level
+  Blynk.virtualWrite(V8, D9);   // Pump 1 Common Fault
+  Blynk.virtualWrite(V9, D10);  // Pump 2 Common Fault
+  Blynk.virtualWrite(V10, D11); // High Level
+  Blynk.virtualWrite(V11, D12); // High Level Backup 1
+  Blynk.virtualWrite(V12, D13); // High Level Backup 2
+  Blynk.virtualWrite(V13, D14); // Long Running
+  Blynk.virtualWrite(V14, D15); // Pump 1 Stopped
+  Blynk.virtualWrite(V15, D16); // Pump 2 Stopped
+  Blynk.virtualWrite(V18, D19); // Water Level Index
+  Blynk.endGroup();
 
   // High level event
   if (D11 > 0 && high_level_flag == false) {
@@ -197,43 +203,30 @@ void myTimer()
     Blynk.logEvent("p2_stopped");
     Serial.println("PUMP 2 STOPPED");
   }
-
-  // Water level indication
-  if (D13 == 1) {
-    D19 = 7;
-  }
-  else if (D12 == 1) {
-    D19 = 6;
-  }
-  else if (D11 == 1) {
-    D19 = 5;
-  }
-  else if (D8 == 1) {
-    D19 = 4;
-  }
-  else if (D5 == 1) {
-    D19 = 2;
-  }
-  else {
-    D19 = 3;
-  }
-  Blynk.virtualWrite(V18, D19);
+  //Serial.println("Wrote DI values to datastreams.");
 }
 
 // Function for checking connection status
 void connectionstatus() {
   Serial.println("Checking connection status...");
   if (WiFi.status() != WL_CONNECTED) {
+    // Reset relays when disconnected from WiFi
+    Serial.println("Reset relays.");
+    P1 = 0;
+    digitalWrite(8, P1);
+    P2 = 0;
+    digitalWrite(12, P2);
     Serial.println("Reconnecting to WiFi...");
     //Blynk.connectWiFi(ssid, pass);
     WiFi.begin(ssid, pass);
     if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Connected to WiFi.");
+      Serial.println("Reconnected to WiFi.");
     }
-  } 
+  }
   else {
     Serial.println("Connected to WiFi.");
   }
+  //Serial.println("Checking is completed.");
 }
 
 // Setup function
@@ -251,8 +244,8 @@ void setup()
   //Blynk.connect();
 
   // Set up Blynk timer object
-  timer.setInterval(1L, myTimer); 
-  timer.setInterval(0.5*60*1000, connectionstatus);
+  timer.setInterval(1000L, myTimer); 
+  timer.setInterval(30000L, connectionstatus);
   
   // Set pin mode for DI pins
   pinMode(A0, INPUT); // Pump 1 Power
@@ -273,33 +266,45 @@ void setup()
   pinMode(12, OUTPUT); // Relay 4 for pump 2
 }
 
-// DO for Pump 1
-BLYNK_WRITE(V0)
-{
-  if (param.asInt() == 1) {
-    digitalWrite(8, HIGH);
-  }
-  else {
-    digitalWrite(8, LOW);
-  }
-  P1 = param.asInt();
-}
-
-// DO for Pump 2
-BLYNK_WRITE(V1)
-{
-  if (param.asInt() == 1) {
-    digitalWrite(12, HIGH);
-  }
-  else {
-    digitalWrite(12, LOW);
-  }
-  P2 = param.asInt();
-}
-
 // Main function
 void loop()
-{ 
+{
+  // Read DI values
+  D3 = digitalRead(A0);  // Pump 1 Power
+  D4 = digitalRead(A1);  // Pump 2 Power
+  D5 = digitalRead(0);   // Low Level
+  D6 = digitalRead(1);   // Pump 1 Running
+  D7 = digitalRead(2);   // Pump 2 Running
+  D8 = digitalRead(3);   // Middle Level
+  D9 = digitalRead(5);   // Pump 1 Common Fault
+  D10 = digitalRead(6);  // Pump 2 Common Fault
+  D11 = digitalRead(9);  // High Level
+  D12 = digitalRead(10); // High Level Backup 1
+  D13 = digitalRead(11); // High Level Backup 2
+  D14 = digitalRead(13); // Long Running
+  D15 = 1 - D6; // Pump 1 Stopped (Opposite of Running)
+  D16 = 1 - D7; // Pump 2 Stopped (Opposite of Running)
+
+  // Determine water level index
+  if (D13 == 1) {
+    D19 = 7;
+  }
+  else if (D12 == 1) {
+    D19 = 6;
+  }
+  else if (D11 == 1) {
+    D19 = 5;
+  }
+  else if (D8 == 1) {
+    D19 = 4;
+  }
+  else if (D5 == 1) {
+    D19 = 2;
+  }
+  else {
+    D19 = 3;
+  }
+
   Blynk.run(); // Run Blynk
   timer.run(); // Run Blynk timer
 }
